30865bda0de072da41d9c29adb70bfa6efce29a3
Karla Elizabeth <elizabeth.lala99@gmail.com>
2022-07-04 20:01:49 -0500

    my proyect

---
+++ src/App.test.js
@@ -0,0 +1,9 @@
+import{render, screen} from '@testing-library/react';
+import App from './App';
+
+test('renders learn reactlink',()=>{
+    render(<App />);
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+
+})
