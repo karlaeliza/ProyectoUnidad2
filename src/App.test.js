d0fef2caba2b30a57c63cbfeb2ca991cda48a6b2
Karla Elizabeth <elizabeth.lala99@gmail.com>
2022-07-04 21:19:57 -0500

    feat: deleit app

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import{render, screen} from '@testing-library/react';
-import App from './App';
-
-test('renders learn reactlink',()=>{
-    render(<App />);
-    const linkElement = screen.getByText(/learn react/i);
-    expect(linkElement).toBeInTheDocument();
-
-})
